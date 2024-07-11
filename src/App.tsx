da060f6ad943b59d1288fe29154fb251c775b48b
Daniel Nicolae <cdn27@cam.ac.uk>
2024-07-11 17:35:17 +0100

    added camera toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,27 @@
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
 import CameraWindow from "./components/CameraWindow"
 
 function App() {
 
+    const currentCameraRef = useRef(0)
 
+    return (
+        <>
+        <div style={{height: 10}}/>
+        <div style={{display: "flex", flexDirection: "row"}}>
 
+            <div style={{width: "25%"}}>
+            </div>
 
+            <div style={{width: "75%"}}>
+                <CameraWindow
+                    currCamRef = {currentCameraRef}/>
+            </div>
 
-  return (
-    <>
-      <div style={{height: 10}}/>
-      <div style={{display: "flex", flexDirection: "row"}}>
-
-        <div style={{width: "50%"}}>
         </div>
-
-        <div style={{width: "50%"}}>
-          <CameraWindow/>
-        </div>
-
-      </div>
-    </>
-  );
+        </>
+    );
 }
 
 export default App;
--- src/components/CameraWindow.tsx
+++ src/components/CameraWindow.tsx
@@ -1,15 +1,50 @@
+import React, { useEffect, useRef, useState } from "react"
 import Webcam from "react-webcam"
 
-const CameraWindow = () => {
 
+interface Props {
+    currCamRef: React.MutableRefObject<number>
+}
+
+const CameraWindow = ({currCamRef}: Props) => {
+
+
+    const [currentCamera, setCurrentCamera] = useState(0)
+    const numberOfDevices = useRef<number>(1)
+    const [cameraIDs, setCameraIDs] = useState<string[]>([])
+
+    const toggleCamera = () => {
+        const nextCamera = (currentCamera + 1) % numberOfDevices.current
+        setCurrentCamera(nextCamera)
+        currCamRef.current = nextCamera
+    }
+
+
+    useEffect(() => {
+        async function getDevices() {
+            const devices = await navigator.mediaDevices.enumerateDevices()
+            const cameraDevices = devices.filter((item) => item.kind === "videoinput")
+            const cameraIDs_temp = cameraDevices.map((item, index) => item.deviceId)
+            console.log(cameraIDs_temp)
+            setCameraIDs(cameraIDs_temp)
+            numberOfDevices.current = cameraIDs_temp.length
+        }
+        getDevices()
+    }, [])
 
 
     return (
+        <>
         <Webcam
             videoConstraints={{
-                aspectRatio: 4/3
+                width: 960,
+                height: 540,
+                aspectRatio: 16/9,
+                deviceId: cameraIDs[currentCamera]
             }}
         /> 
+        <button onClick={toggleCamera}>toggle camera</button>
+        </>
     )
 }
 
