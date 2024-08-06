b38085508a8953c9defc015d143b09a6b60107f2
Daniel Nicolae <cdn27@cam.ac.uk>
2024-08-06 18:03:58 +0100

    corrected head rendering camera position

--- src/components/HeadRendering.tsx
+++ src/components/HeadRendering.tsx
@@ -36,7 +36,7 @@ const HeadRendering = ({ear, matrixRef}: Props) => {
 
         // Camera initialisation
         camera.current = new THREE.PerspectiveCamera(5, 1)
-        camera.current.position.set(0, 50, 250) 
+        camera.current.position.set(0, 0, 250) 
         camera.current.lookAt(0, 0, 0)
 
 
@@ -75,7 +75,7 @@ const HeadRendering = ({ear, matrixRef}: Props) => {
         // Load head mesh
         loader.load("meshes/head.ply", (geometry) => {
 
-            const material = new THREE.MeshPhongMaterial({color: 0x555555, flatShading: true, transparent: true, opacity: 0.98})
+            const material = new THREE.MeshPhongMaterial({color: 0x555555, flatShading: true, transparent: true, opacity: 0.9})
             const loadedMesh = new THREE.Mesh(geometry.center(), material)
 
             scene.current!.add(loadedMesh)
